2eed271c948849d78305801fe983abece77a187b
mauvlad <nitrox-tk@hotmail.com>
2019-02-08 16:14:59 -0600

    uso de angular y firebase ejemplo de uso de firebase con angular ang-fb-mod: carpeta usada para trabajar con el firebase. ang-fb-orig: carpeta con el ejemplo original de firebase.

---
+++ test2/ang-fb-mod/src/environments/environment.ts
@@ -0,0 +1,23 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBkXfMTa0_fnPlNA3Tl10N5_CTHqQSoouo",
+    authDomain: "ang-fb-cbf2c.firebaseapp.com",
+    databaseURL: "https://ang-fb-cbf2c.firebaseio.com",
+    projectId: "ang-fb-cbf2c",
+    storageBucket: "ang-fb-cbf2c.appspot.com",
+    messagingSenderId: "1025501819247"
+  }
+};
+
+/*
+ * In development mode, to ignore zone related error stack frames such as
+ * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
+ * import the following file, but please comment it out in production mode
+ * because it will have performance impact when throw error
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
